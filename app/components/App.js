4a7fcd1f212348f7fe689cfe7f44ebf339ff73bf
Rico <richardthomaswhitfield@gmail.com>
2017-04-14 13:00:53 -0500

    added popluar compnent

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,12 @@
 var React = require('react');
+var Popular = require('./Popular');
 
 class App extends React.Component {
 
 	render() {
 		return (
 			<div>
-				Hello!
+				<Popular />
 			</div>
 			)
 	}

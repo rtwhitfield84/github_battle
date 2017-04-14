36285410a0d36e1e18f0d777ab4834a625ac81f7
Rico <richardthomaswhitfield@gmail.com>
2017-04-14 13:10:58 -0500

    update popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,8 +2,17 @@ var React = require('react');
 
 class Popular extends React.Component {
 	render() {
+		var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 		return (
-			<div>Popular!</div>
+			<ul className='languages'>
+				{languages.map((lang) => {
+					return (
+						<li key={lang}>
+							{lang}
+						</li>
+					)
+				})}
+			</ul>
 		)
 	}
 }

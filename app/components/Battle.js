c6409c55acfa90b90ebe261efcae3fda288911a3
Rico <richardthomaswhitfield@gmail.com>
2017-04-18 14:16:08 -0500

    added player input component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,63 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			username: ''
+		};
+
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+
+	handleChange(event){
+		var value= event.target.value;
+
+		this.setState(() => {
+			return {
+				username: value
+			}
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.onSubmit(
+			this.props.id,
+			this.state.username
+		)
+	}
+	render() {
+		return (
+			<form className='column' onSubmit={this.handleSubmit}>
+				<label className='header' htmlFor='username'>
+					{this.props.label}
+				</label>
+				<input id='username' 
+				placeholder='GitHub Username'
+				type='text'
+				autoComplete='off'
+				value={this.state.username}
+				onChange={this.handleChange}/>
+			<button className='button' type='submit' disabled={!this.state.username}>
+			Submit
+			</button>
+			</form>
+
+		)
+	}
+}
+
+PlayerInput.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	onSubmit: PropTypes.func.isRequired
+}
 
 class Battle extends React.Component {
 	constructor(props) {
@@ -10,6 +69,8 @@ class Battle extends React.Component {
 			playerOneImage: null,
 			playerTwoImage: null
 		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleSubmit(id, username) {
@@ -22,9 +83,19 @@ class Battle extends React.Component {
 		});
 	}
 	render() {
+		var playerOneName = this.state.playerOneName;
+		var playerTwoName = this.state.playerTwoName;
 		return(
 			<div>
-			Battle!
+
+				<div className='row'>
+				{!playerOneName &&
+					<PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit} />}
+
+				{!playerTwoName &&
+					<PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit} />}
+
+				</div>
 			</div>
 		)
 	}

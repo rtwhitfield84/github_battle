48bd4646578f41bb61133b1f031f888e4af610bc
Rico <richardthomaswhitfield@gmail.com>
2017-04-18 15:44:11 -0500

    updated battle component and added results

--- app/components/App.js
+++ app/components/App.js
@@ -7,6 +7,8 @@ var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Battle = require('./Battle');
+var Results = require('./Results');
 
 class App extends React.Component {
 
@@ -18,6 +20,7 @@ class App extends React.Component {
 				<Switch> 
 				<Route exact path='/' component={Home} />
 				<Route  exact path='/battle' component={Battle} />
+				<Route path='/battle/results' component={Results} />
 				<Route path='/popular' component={Popular} />
 				<Route render={() => {
 					return <p>Not Found</p>

44240d3cd630442a8c957596189edd474806c73a
Rico <richardthomaswhitfield@gmail.com>
2017-04-18 13:39:53 -0500

    handle submit method

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,26 @@
 var React = require('react');
 
 class Battle extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			playerOneName: '',
+			playerTwoName: '',
+			playerOneImage: null,
+			playerTwoImage: null
+		}
+	}
+
+	handleSubmit(id, username) {
+		this.setState(() => {
+			var newState = {};
+
+			newState[id + 'Name'] = username;
+			newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+			return newState;
+		});
+	}
 	render() {
 		return(
 			<div>

35762b03e76e23ec053feb10f3d13726d867159d
Rico <richardthomaswhitfield@gmail.com>
2017-04-19 13:37:26 -0500

    getRepos

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,17 +1,20 @@
 var axios = require('axios');
 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+//in case of rate limiting
+// var id = "YOUR_CLIENT_ID";
+// var sec = "YOUR_SECRET_ID";
+// var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile(username) {
-	return axios.get('https://api.github.com/users/' + username + params)
+	return axios.get('https://api.github.com/users/' + username)
 	.then((user) => {
 		return user.data
 	});
 }
 
-
+function getRepos(username) {
+	return axios.get('https://api.github.com/users/' + username + '/repos?&per_page=100')
+}
 module.exports = {
 	battle: function(players) {
 

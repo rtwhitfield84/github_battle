f1f5261c58bbe8264fb2bcfa2e5adb81f47b6ee7
Rico <richardthomaswhitfield@gmail.com>
2017-04-21 11:48:48 -0500

    loading style

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,10 +1,19 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+var styles = {
+	content: {
+		textAlign: 'center',
+		fontSize: '35px'
+	}
+}
+
 class Loading extends React.Component {
 	render() {
 		return (
-
+			<p style={styles.content}>
+				{this.state.text}
+			</p>
 		)
 	}
 }
